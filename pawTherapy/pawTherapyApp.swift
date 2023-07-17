ea8591e01dc4ba21b6329cdc23d667d06d729dfe
Bread Beisheim <catcoder01@gmail.com>
2023-07-17 15:31:05 -0500

    Initial Commit

---
+++ pawTherapy/pawTherapyApp.swift
@@ -0,0 +1,17 @@
+//
+//  pawTherapyApp.swift
+//  pawTherapy
+//
+//  Created by Scholar on 7/17/23.
+//
+
+import SwiftUI
+
+@main
+struct pawTherapyApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
