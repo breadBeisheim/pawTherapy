bfb0a5fd8934ee3d50cedc15675ae102db9baf22
Bread Beisheim <catcoder01@gmail.com>
2023-07-18 15:35:43 -0500

    paan therapy our app that has a homepage

--- pawTherapy/ContentView.swift
+++ pawTherapy/ContentView.swift
@@ -9,18 +9,69 @@ import SwiftUI
 
 struct ContentView: View {
     var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundColor(.accentColor)
-            Text("Hello, world!")
-        }
-        .padding()
-    }
-}
+        NavigationStack {
+            VStack {
+                Text("Paw Therapy")
+                    .font(.largeTitle)
+                    .fontWeight(.light)
+                   
+            
+                    
+                        .padding(0)
+                    
+                    Text("Click on me to try a fun scavenger hunt!")
+                        .font(.subheadline)
+                        .padding(0)
+                
+                HStack {
+                    Text("Click on me to try a challenging puzzle!")
+                        .font(.subheadline)
+                        .padding(0)
+                    
+                    
+                    
+                }
+                HStack {
+                    
+                    
+                    Text("Click on me to try a mid boggling riddle!")
+                        .font(.subheadline)
+                    
+                    Text("Click on me to try a relaxing meditation!")
+                        .font(.subheadline) }
+                
+                HStack {
+                    
+                    Image("blackcat")
+                        .resizable(resizingMode: .stretch)
+                        .aspectRatio(contentMode: .fit)
+                    Image("greycat2")
+                        .resizable(resizingMode: .stretch)
+                        .aspectRatio(contentMode: .fit)
 
-struct ContentView_Previews: PreviewProvider {
-    static var previews: some View {
-        ContentView()
+                    Image("gingercat2")
+                        .resizable(resizingMode: .stretch)
+                        .aspectRatio(contentMode: .fit)
+                    
+                    Image("pinkishcat2")
+                        .resizable(resizingMode: .stretch)
+                        .aspectRatio(contentMode: .fit)
+                    
+                    
+                                    }
+                   
+               
+                
+                
+                
+                
+            }
+               }
+    }
+    
+    struct ContentView_Previews: PreviewProvider {
+        static var previews: some View {
+            ContentView()
+        }
     }
 }
